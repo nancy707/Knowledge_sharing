98250e60e536a41fa2bfcb54335cd8a3d74b0d56
nancy707 <31887214+nancy707@users.noreply.github.com>
2018-05-26 15:57:36 +0530

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2018 at 12:26 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `books`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_details`
+--
+
+CREATE TABLE `book_details` (
+  `bid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` varchar(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `dept` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book_details`
+--
+
+INSERT INTO `book_details` (`bid`, `name`, `author`, `category`, `price`, `uid`, `dept`) VALUES
+(6, 'fwgiu', 'ijjjni', 'technical', '135', 1, 'CP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credentials`
+--
+
+CREATE TABLE `credentials` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` bigint(20) NOT NULL,
+  `dept` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`id`, `name`, `email`, `password`, `contact`, `dept`) VALUES
+(1, 'nancy', 'nancygarala23@gmail.com', '$2y$10$BD093p7x7paHhq0r1kHdleCzb2qJyANyQCr3kXdl38.6NMEnw/1Jy', 7894561230, 'CP');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_details`
+--
+ALTER TABLE `book_details`
+  ADD PRIMARY KEY (`bid`);
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_details`
+--
+ALTER TABLE `book_details`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
