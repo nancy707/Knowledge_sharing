55129cb9b81c7bdc85fdf995bc82e903c10af8e0
unknown <nancy707>
2020-09-09 22:51:43 +0530

    Removed old data

--- dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-//connect to mysql database
-$con =  mysqli_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
-?>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    header('Location: jobs.php');
-    exit;
-?>
